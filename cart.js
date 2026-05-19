0101639513b829992d0ddb64df13a1db896270f9
Priya <mstpriyaakter724@gmail.com>
2026-05-19 21:02:40 +0600

    add subtotal, discount, total and navbar

--- cart.js
+++ cart.js
@@ -2,13 +2,13 @@ print_cart();
 subtotal();
 item_count()
 
-function print_cart(){
-    let cart = JSON.parse(localStorage.getItem("product_cart"))?? [];
-let html = "";
-cart.forEach((product)=>{
-    console.log(product);
-    
-html+=`
+function print_cart() {
+    let cart = JSON.parse(localStorage.getItem("product_cart")) ?? [];
+    let html = "";
+    cart.forEach((product) => {
+        console.log(product);
+
+        html += `
             <div class="product-card p-3 shadow-sm">
                             <div class="row align-items-center">
                                 <div class="col-md-2">
@@ -33,12 +33,12 @@ html+=`
                                     </div>
                                      <div>
                                         <span class="fw-bold">$${product.price * product.qty}</span>
-                                        <span class="">Subtotal</span>
+                                        <span class="ms-2">      Subtotal</span>
                                     </div>
 
                                 </div>
-                                <div class="col-md-1" onclick= "deleteItem(${product.id})">
-                                    <i class="bi bi-trash remove-btn"></i>
+                                <div class="col-md-1" >
+                                    <i class="bi bi-trash remove-btn"onclick= "deleteItem(${product.id})" ></i>
                                 </div>
                             </div>
                         </div>
@@ -46,10 +46,97 @@ html+=`
 
 `;
 
-});
+    });
 
-document.querySelector(".show_products").innerHTML= html;
-let item = cart.length > 1 ? "items": "item";
-document.querySelector(".item_count").innerText= cart.length + " " + item;
-item_count()
+    document.querySelector(".show_products").innerHTML = html;
+    let item = cart.length > 1 ? "items" : "item";
+    document.querySelector(".item_count").innerText = cart.length + " " + item;
+    item_count()
+}
+
+function subtotal() {
+    let cart = JSON.parse(localStorage.getItem("product_cart")) ?? [];
+    console.log(cart);
+
+
+    total = cart.reduce((acc, item) => acc += (item.price * item.qty), 0);
+    document.querySelector("#subtotal").innerText = `$${total.toFixed(2)}`;
+
+    let discount_amount = discount(total);
+    document.querySelector("#discount").innerText = `-$${discount_amount.toFixed(2)}`;
+
+    let final = finalTotal(total, discount_amount);
+    // document.querySelector("#total").innerText = `$${total - discount_amount.toFixed(2)}`;
+    document.querySelector("#total").innerText = `$${final.toFixed(2)}`;
+
+}
+
+function discount(total) {
+
+    if (total >= 2000) {
+        return total * 0.20;
+    }
+    else if (total >= 1500) {
+        return total * 0.10;
+    }
+    else if (total >= 1000) {
+        return total * 0.05;
+    }
+
+    return 0;
+}
+
+
+function finalTotal(total, discount_amount, shipping = 5){
+    return total - discount_amount + shipping;
+}
+
+function updateQuantityInc(id) {
+    console.log(id);
+    let cart = JSON.parse(localStorage.getItem("product_cart")) ?? [];
+
+    cart = cart.map((item) => {
+        if (item.id == id) {
+            return { ...item, qty: item.qty += 1 }
+        } else {
+            return item;
+        }
+    });
+    localStorage.setItem("product_cart", JSON.stringify(cart))
+    print_cart()
+    subtotal()
+}
+
+function updateQuantityDec(id) {
+    let cart = JSON.parse(localStorage.getItem("product_cart")) ?? [];
+
+    cart = cart.map((item) => {
+        if (item.id == id && item.qty > 1) {
+            return { ...item, qty: item.qty -= 1 }
+        } else {
+            return item;
+        }
+    })
+    localStorage.setItem("product_cart", JSON.stringify(cart))
+    print_cart()
+    subtotal()
+}
+
+function deleteItem(id) {
+    let cart = JSON.parse(localStorage.getItem("product_cart")) ?? [];
+    cart = cart.filter((item) => item.id != id);
+    localStorage.setItem("product_cart", JSON.stringify(cart))
+    print_cart()
+    subtotal()
+}
+
+function item_count() {
+    let cart = JSON.parse(localStorage.getItem("product_cart"));
+    if (cart.length > 0) {
+        document.querySelector(".cart_items_count").innerHTML = `
+        <span class="position-absolute top-0 start-100 translate-middle badge badge-sm rounded-pill bg-danger">
+            ${cart.length}
+           </span>
+        `;
+    }
 }
